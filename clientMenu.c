d6667941aaaa9fc7fa59ee9a3321e61292684f8f
Michael Carrier <michael.carrier@pdt.com>
2012-04-30 11:38:22 -0700

    RECONN-227

---
+++ clientMenu.c
@@ -0,0 +1,139 @@
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+
+#include "debugMenu.h"
+#include "reconn.h"
+
+static int iPhoneInserted = -1;
+
+extern void reconnMasterIphone();
+extern int theDebugSocketFd;
+
+static int iPhoneInsert();
+static int iPhoneExtract();
+static int clientMode();
+static int masterMode();
+static int numberClients();
+static int upgrade();
+
+debugMenuStruct clientDebugMenu[] =
+{
+    {"client", "Client debug Menus", NULL, NULL, NULL},
+#ifdef __SIMULATION__
+    {NULL, NULL, "insert", "Simulates front panel iPhone insertion", iPhoneInsert},
+    {NULL, NULL, "extract", "Simulates front panel iPhone extraction", iPhoneExtract},
+    {NULL, NULL, "client", "Simulates USB client access request", clientMode},
+    {NULL, NULL, "master", "Simulates USB Master mode request", masterMode},
+#endif
+    {NULL, NULL, "sac", "Show the number of active client sessions", numberClients},
+    {NULL, NULL, "upgrade", "Execute a software upgrade", upgrade},
+};
+
+void registerClientDebugMenu()
+{
+    registerDebugCommand(&clientDebugMenu[0], sizeof(clientDebugMenu)/sizeof(debugMenuStruct));
+}
+
+static int iPhoneInsert()
+{
+    iPhoneInserted = 0;
+    reconnMasterIphone();
+    return RECONN_SUCCESS;
+}
+
+static int iPhoneExtract()
+{
+    iPhoneInserted = -1;
+    reconnMasterIphone();
+    return RECONN_SUCCESS;
+}
+
+int clientMode()
+{
+    ReconnPacket thePacket;
+    ReconnPacket *thePacketPtr = &thePacket;
+
+    ADD_MSGID_TO_PACKET(CLIENT_ACCESS_REQ, thePacketPtr);
+    ADD_DATA_LENGTH_TO_PACKET(0, thePacketPtr);
+    insertedMasterRead(&thePacket, 4);
+    return RECONN_SUCCESS;
+}
+
+int masterMode()
+{
+    ReconnPacket thePacket;
+    ReconnPacket *thePacketPtr = &thePacket;
+
+    ADD_MSGID_TO_PACKET(MASTER_MODE_REQ, thePacketPtr);
+    ADD_DATA_LENGTH_TO_PACKET(0, thePacketPtr);
+    insertedMasterRead(&thePacket, 4);
+    return RECONN_SUCCESS;
+}
+
+int numberClients()
+{
+    char outbuf[DEBUG_OUTPUT_LEN];
+
+    memset((char *)&outbuf, 0, DEBUG_OUTPUT_LEN);
+    sprintf((char *)&outbuf, "The number of active clients is  %d\r\n", reconnClientsRegistered());
+    if(send(theDebugSocketFd, outbuf, strlen(outbuf)) <= 0)
+    {
+        printf("%s: send failed %d(%s)\n", __FUNCTION__, errno, strerror(errno));
+    }
+    return RECONN_SUCCESS;
+}
+
+int upgrade(void)
+{
+    char outbuf[DEBUG_OUTPUT_LEN];
+    ReconnErrCodes retCode;
+
+    if(reconnClientsRegistered() > 0)
+    {
+        sprintf((char *)&outbuf, "Can't upgrade because there are %d active clients.\r\n", reconnClientsRegistered());
+        send(theDebugSocketFd, (char *)&outbuf, strlen((char *)&outbuf));
+    }
+    else
+    {
+        if((retCode = extractBundle()) == RECONN_SUCCESS)
+        {
+            system("killall iphoned");
+            system("killall reconn-service");
+        }
+        else
+        {
+            memset((char *)&outbuf, 0, DEBUG_OUTPUT_LEN);
+            switch (retCode)
+            {
+                case RECONN_UPGRADE_FILE_NOT_FOUND:
+                {
+                    strcpy((char *)&outbuf, "UPGRADE ABORTED: upgrade file not found.\n");
+                    send(theDebugSocketFd, &outbuf, strlen(outbuf));
+                    break;
+                }
+                case RECONN_UPGRADE_BAD_CHECKSUM:
+                {
+                    strcpy((char *)&outbuf, "UPGRADE ABORTED: /tmp/upgradeBundle has an invalid checksum.\n");
+                    send(theDebugSocketFd, &outbuf, strlen(outbuf));
+                    break;
+                }
+                default:
+                {
+                    strcpy((char *)&outbuf, "UPGRADE ABORTED: ");
+                    strcat((char *)&outbuf, strerror(errno));
+                    strcat((char *)&outbuf, "\n"); 
+                    send(theDebugSocketFd, &outbuf, strlen(outbuf));
+                    break;
+                }
+            }
+        }
+    }
+    return RECONN_SUCCESS;
+}
+#ifdef __SIMULATION__
+int simulate_isiphonepresent()
+{
+    return iPhoneInserted;
+}
+#endif
